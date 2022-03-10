4ffbd406dfa496bc94814e30e5776b6f7f9d64f6
Tao Wen <taowen@chengfayun.com>
2022-03-10 17:20:30 +0800

    servePage

--- packages/vue-fusion/src/servePage.ts
+++ packages/vue-fusion/src/servePage.ts
@@ -1,6 +1,6 @@
-import { extractScripts } from "./extractScripts";
 import * as fs from 'fs';
 import * as path from 'path';
+import { extractScripts } from "./extractScripts";
 
 export function servePage(renderResult: { mpData: any }, htmlFile: string) {
     const page: Record<string, any> = { ...renderResult };
@@ -8,8 +8,7 @@ export function servePage(renderResult: { mpData: any }, htmlFile: string) {
         page.scripts = {};
     }
     const html = fs.readFileSync(htmlFile, { encoding: 'utf-8'});
-    const scripts = extractScripts(html);
-    for (const script of scripts) {
+    for (const script of extractScripts(html)) {
         const scriptFile = path.join(path.dirname(htmlFile), script);
         const scriptContent = fs.readFileSync(scriptFile, { encoding: 'utf-8'});
         page.scripts[script] = scriptContent;
