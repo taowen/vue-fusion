a91d7080b2266e12e1988dafd34172d0a388e5eb
Tao Wen <taowen@chengfayun.com>
2022-03-11 22:10:52 +0800

    proof of concept

--- packages/vue-fusion-weapp/pages/GenericPage.js
+++ packages/vue-fusion-weapp/pages/GenericPage.js
@@ -1,15 +1,58 @@
 // index.ts
 // 获取应用实例
 const app = getApp()
+const def = require('define-function');
+const ctx = def.context({
+  global: { console, fwx: {
+    setMpData(pageId, node) {
+      console.log(pageId, node, new Date().getTime());
+        }
+  } },
+  async dynamicImport(basename, filename) {
+    let { data } = await new Promise((resolve, reject) => wx.request({
+      url: `http://localhost:3000${filename}`,
+      success: resolve,
+      error: reject
+    }))
+    return data;
+  }
+});
+
+
+
+async function loadScript(script) {
+  let { data } = await new Promise((resolve, reject) => wx.request({
+    url: `http://localhost:3000${script}`,
+    success: resolve,
+    error: reject
+  }))
+  await ctx.load(data);  
+}
 
 Page({
   async onLoad() {
-    const { data } = await new Promise((resolve, reject) => wx.request({
+    let { data } = await new Promise((resolve, reject) => wx.request({
       url: 'http://localhost:3000',
       success: resolve,
       error: reject
     }))
-    this.setData({ fragments: data.mpData })
+    const end = data.indexOf('</html>');
+    if (end !== -1) {
+      data = data.substring(end + '</html>'.length);
+    }
+    const { scripts, mpData } = JSON.parse(data);
+    this.setData({ fragments: mpData });
+    for (const script of Object.keys(scripts)) {
+      await loadScript(script);
+    }
+    console.log('loaded', new Date().getTime());
+    const renderPage = await ctx.def(`
+return (async() => {
+  const client = await import('/src/entry-client.ts');
+  return client.renderPage(...arguments);
+})();
+`)
+await renderPage('page1');
     // this.setData({
     //   nodes: [{
     //     type: 'swiper',
