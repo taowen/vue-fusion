8baf3175e5ac8f1cd39f9c7aee2dc1ede7d93c74
Tao Wen <taowen@chengfayun.com>
2022-04-08 11:49:32 +0800

    render 3 months

--- packages/demo-huangli/src/pages/Calendar/Calendar.tsx
+++ packages/demo-huangli/src/pages/Calendar/Calendar.tsx
@@ -4,25 +4,52 @@ import { range } from '../../shared/range';
 import RoundBox from '../../shared/RoundBox';
 import CalendarMonth from './CalendarMonth';
 
+class Month {
+    constructor(public year: number, public month: number) {
+    }
+    get next() {
+        let year = this.year;
+        let month = this.month + 1;
+        if (month === 12) {
+            year += 1;
+            month = 0;
+        }
+        return new Month(year, month);
+    }
+    get prev() {
+        let year = this.year;
+        let month = this.month - 1;
+        if (month === -1) {
+            year -= 1;
+            month = 11;
+        }
+        return new Month(year, month);
+    }
+    get id() {
+        return `m-${this.year}-${this.month}`
+    }
+}
+
 export default fusion.defineComponent({
     data() {
         const today = new Date();
+        const todayMonth = new Month(today.getFullYear(), today.getMonth());
         return {
-            year: today.getFullYear(),
-            month: today.getMonth(),
+            months: [todayMonth.prev, todayMonth, todayMonth.next],
+            currentMonth: todayMonth
         }
     },
     render() {
         let lastEventTime = new Date().getTime();
         return <RoundBox>
                 {() => <view class="flex-col gap-2">
-                <view>{`${this.year}年${this.month + 1}月`}</view>
+                <view>{`${this.currentMonth.year}年${this.currentMonth.month + 1}月`}</view>
                 <view class="flex-row w-full justify-around">
                     {range(7, i =>
                         <view class={i === 5 || i === 6 ? 'color-2' : 'color-1'}>
                             {['一', '二', '三', '四', '五', '六', '日'][i]}
                         </view>)}</view>
-                    <scroll-view id="the-scroll-view" scroll-x enhanced paging-enabled style="white-space: nowrap;" scroll-into-view="middlePage" onScrolltolower={(e) => {
+                    <scroll-view id="the-scroll-view" scroll-x enhanced paging-enabled style="white-space: nowrap;" scroll-into-view={this.currentMonth.id} onScrolltolower={(e) => {
                         if (new Date().getTime() - lastEventTime < 1000) {
                             return;
                         }
@@ -35,9 +62,7 @@ export default fusion.defineComponent({
                         lastEventTime = new Date().getTime();
                         console.log('scroll to upper', e);
                     }}>
-                        <view class="w-full" style="display: inline-flex;">上个月</view>
-                        <CalendarMonth id="middlePage" year={this.year} month={this.month}/>
-                        <view style="display: inline-flex; width: 100%;">下个月</view>
+                        {this.months.map(m => <CalendarMonth id={m.id} year={m.year} month={m.month}/>)}
                     </scroll-view>
                 </view>}
         </RoundBox>
